217e98d48a2e3afd568d6a121b02bef2e05443a5
mrs433d <154293128+mrs433d@users.noreply.github.com>
2025-05-23 14:11:51 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
+# config.py
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
